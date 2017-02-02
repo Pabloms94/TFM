965bf2262fc164dd4058a88e1e37bdcf88489c5a
Pablo <pabloms.94@usal.es>
2017-02-02 13:58:50 +0100

    Cámara y puente

--- Cueva/Assets/Scripts/ScriptRock.cs
+++ Cueva/Assets/Scripts/ScriptRock.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScriptRock : MonoBehaviour {
-
+    private Rigidbody rockRigidbody;
+    public GameObject brigde;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        rockRigidbody = GetComponent<Rigidbody>();
+        //brigde = GameObject.FindGameObjectWithTag("Bridge");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if(transform.position.y < )
+        if (transform.position.y < -0.9)
+        {
+            rockRigidbody.constraints = RigidbodyConstraints.FreezeAll;
+            brigde.SetActive(true);
+            Debug.Log(brigde.activeSelf);
+        }
 	}
 }
